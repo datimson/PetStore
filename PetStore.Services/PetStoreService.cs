318b3dbc2515ebb45a4494e655c7a427e9358ec7
Dean <me@deantimson.com>
2020-03-19 14:25:10 +1000

    created GetAvailablePets that uses the PetstoreClient

--- PetStore.Services/PetStoreService.cs
+++ PetStore.Services/PetStoreService.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PetStore.Services
 {
     public class PetStoreService
     {
+        private readonly IPetstoreClient _client;
+        public PetStoreService(IPetstoreClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<Pet>> GetAvailablePets()
+        {
+            var pets = await _client.FindPetsByStatusAsync(new[] { Anonymous.Available });
+
+            if (pets == null)
+            {
+                return new List<Pet>();
+            }
+
+            return pets.ToList();
+        }
     }
 }
--- PetStore.Tests/PetStoreServiceTests.cs
+++ PetStore.Tests/PetStoreServiceTests.cs
@@ -1,14 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using PetStore.Services;
 using Xunit;
 
 namespace PetStore.Tests
 {
     public class PetStoreServiceTests
     {
-        [Fact]
-        public void Test1()
+        public class GetAvailablePets
         {
+            [Fact]
+            public async Task WhenNoPets_ThenReturnEmptyList()
+            {
+                var mockPetstoreClient = new Mock<IPetstoreClient>();
+                mockPetstoreClient.Setup(x => x.FindPetsByStatusAsync(new[] { Anonymous.Available })).ReturnsAsync(null as ICollection<Pet>);
 
+                var service = new PetStoreService(mockPetstoreClient.Object);
+                var pets = await service.GetAvailablePets();
+
+                Assert.NotNull(pets);
+                Assert.Empty(pets);
+            }
+
+            [Fact]
+            public async Task WhenPets_ThenReturnList()
+            {
+                var mockPetstoreClient = new Mock<IPetstoreClient>();
+                var pets = new Pet[] {
+                    new Pet { Id = 1, Name = "Test 1", Status = PetStatus.Available },
+                    new Pet { Id = 2, Name = "Test 2", Status = PetStatus.Available }
+                };
+                mockPetstoreClient.Setup(x => x.FindPetsByStatusAsync(new[] { Anonymous.Available })).ReturnsAsync(pets);
+
+                var service = new PetStoreService(mockPetstoreClient.Object);
+                var petsResult = await service.GetAvailablePets();
+
+                Assert.NotNull(petsResult);
+                Assert.Equal(pets, petsResult);
+            }
         }
     }
 }

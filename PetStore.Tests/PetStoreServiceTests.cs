a5409099c95e78f1bcfbf3b29102a006296b8bd9
Dean <me@deantimson.com>
2020-03-19 14:11:53 +1000

    created the solution with Console, Services and Tests projects

---
+++ PetStore.Tests/PetStoreServiceTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PetStore.Tests
+{
+    public class PetStoreServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
